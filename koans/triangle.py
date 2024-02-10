9f61e7e9d5138400be01972dd46e9ceeb5094442
Brian Wakabi <b.wakabi@corp.harissint.com>
2024-02-10 19:46:33 +0300

    Finished About Triangles

--- koans/triangle.py
+++ koans/triangle.py
@@ -18,8 +18,16 @@
 #
 def triangle(a, b, c):
     # DELETE 'PASS' AND WRITE THIS CODE
-    pass
+    if (a == b == c):
+        return 'equilateral'
+    elif (a == b or a == c or b == c):
+        return 'isosceles'
+    else:
+        return 'scalene'
+
 
 # Error class used in part 2.  No need to change this code.
+
+
 class TriangleError(Exception):
     pass
